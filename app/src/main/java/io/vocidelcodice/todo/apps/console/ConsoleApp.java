961d4f2463513ef1f3bcaa454310b9ba17ffc750
Pietro Di Bello <pierodibello@gmail.com>
2021-04-28 23:13:29 +0200

    add the main for the console app

--- app/src/main/java/io/vocidelcodice/todo/apps/console/ConsoleApp.java
+++ app/src/main/java/io/vocidelcodice/todo/apps/console/ConsoleApp.java
@@ -1,8 +1,28 @@
 package io.vocidelcodice.todo.apps.console;
 
+import io.vocidelcodice.todo.addtodo.controller.AddTodoController;
+import io.vocidelcodice.todo.addtodo.persistence.InMemoryAddTodoDataAccessInterface;
+import io.vocidelcodice.todo.addtodo.presenter.AddTodoPresenter;
+import io.vocidelcodice.todo.addtodo.presenter.AddTodoViewModel;
+import io.vocidelcodice.todo.addtodo.usecase.AddTodoDataAccessInterface;
+import io.vocidelcodice.todo.addtodo.usecase.AddTodoInputBoundary;
+import io.vocidelcodice.todo.addtodo.usecase.AddTodoOutputBoundary;
+import io.vocidelcodice.todo.addtodo.usecase.AddTodoUseCase;
+import io.vocidelcodice.todo.apps.console.addtodo.view.AddTodoView;
+
 public class ConsoleApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        AddTodoDataAccessInterface dataAccessInterface = new InMemoryAddTodoDataAccessInterface();
+        AddTodoView view = new AddTodoView();
+
+        AddTodoViewModel viewModel = new AddTodoViewModel();
+        viewModel.addObserver(view);
+        AddTodoOutputBoundary addTodoPresenter = new AddTodoPresenter(viewModel);
+
+        AddTodoInputBoundary addTodoUseCase = new AddTodoUseCase(dataAccessInterface, addTodoPresenter);
+
+        AddTodoController controller = new AddTodoController(addTodoUseCase);
+        controller.addTodo(args[0]);
     }
 }
