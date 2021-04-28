843e448a623b572267b6bde6f3ddfcaf52e5726e
Matteo Pierro <pierromatteo@gmail.com>
2021-04-28 23:48:28 +0300

    implement persistence layer

---
+++ app/src/main/java/io/vocidelcodice/todo/addtodo/persistence/InMemoryAddTodoDataAccessInterface.java
@@ -0,0 +1,16 @@
+package io.vocidelcodice.todo.addtodo.persistence;
+
+import io.vocidelcodice.todo.addtodo.entities.Todo;
+import io.vocidelcodice.todo.addtodo.usecase.AddTodoDataAccessInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemoryAddTodoDataAccessInterface implements AddTodoDataAccessInterface {
+    private static final List<Todo> TODOS = new ArrayList<>();
+
+    @Override
+    public void add(Todo todo) {
+        TODOS.add(todo);
+    }
+}
