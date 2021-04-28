35f6212f117307383118526b9676fa27d2da494c
Pietro Di Bello <pierodibello@gmail.com>
2021-04-28 23:17:15 +0200

    add a comment to describe the different modules

--- app/src/main/java/io/vocidelcodice/todo/apps/console/ConsoleApp.java
+++ app/src/main/java/io/vocidelcodice/todo/apps/console/ConsoleApp.java
@@ -1,15 +1,25 @@
 package io.vocidelcodice.todo.apps.console;
 
+// controller module
 import io.vocidelcodice.todo.addtodo.controller.AddTodoController;
+
+// database adapter module
 import io.vocidelcodice.todo.addtodo.persistence.InMemoryAddTodoDataAccessInterface;
+
+// presenter module
 import io.vocidelcodice.todo.addtodo.presenter.AddTodoPresenter;
 import io.vocidelcodice.todo.addtodo.presenter.AddTodoViewModel;
+
+// usecase module
 import io.vocidelcodice.todo.addtodo.usecase.AddTodoDataAccessInterface;
 import io.vocidelcodice.todo.addtodo.usecase.AddTodoInputBoundary;
 import io.vocidelcodice.todo.addtodo.usecase.AddTodoOutputBoundary;
 import io.vocidelcodice.todo.addtodo.usecase.AddTodoUseCase;
+
+// console view module
 import io.vocidelcodice.todo.apps.console.addtodo.view.AddTodoView;
 
+// main module
 public class ConsoleApp {
 
     public static void main(String[] args) {
